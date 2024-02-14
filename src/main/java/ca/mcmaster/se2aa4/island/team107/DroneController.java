e71cc81ba55742f50c47353d408b7616446eda3a
chosterto <johnnychox@gmail.com>
2024-02-13 23:23:27 -0500

    Added DroneController

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team107/DroneController.java
@@ -0,0 +1,19 @@
+package ca.mcmaster.se2aa4.island.team107;
+
+import org.json.JSONObject;
+
+public class DroneController {
+    private Drone drone;
+
+    public DroneController(Drone drone) {
+        this.drone = drone;
+    }
+
+    public String runCommand() {
+        return "";
+    }
+
+    public String updateDrone(JSONObject output) {
+        return "";
+    }
+}
