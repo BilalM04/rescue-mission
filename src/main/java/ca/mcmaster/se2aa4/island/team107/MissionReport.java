f2430b95e3fbefda9b2c8dfc02ee4f2fbaa08b1a
bilalm04 <bilal71612@gmail.com>
2024-03-17 22:48:59 -0400

    created mission report class close #63

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team107/MissionReport.java
@@ -0,0 +1,44 @@
+package ca.mcmaster.se2aa4.island.team107;
+
+import java.util.NoSuchElementException;
+import ca.mcmaster.se2aa4.island.team107.Position.Map;
+
+public class MissionReport implements Report {
+    
+    Map map;
+
+    public MissionReport(Map map) {
+        this.map = map;
+    }
+
+    public String generateReport() {
+        StringBuilder report = new StringBuilder();
+        report.append("Emergency Site ID: ").append(emergencySiteID()).append("\n");
+        report.append("Closest Creek ID: ").append(closestCreekID()).append("\n");
+        return report.toString();
+    }
+
+    private String closestCreekID() {
+        String creekID;
+
+        try {
+            creekID = map.getClosestCreekID();
+        } catch (NoSuchElementException e) {
+            creekID = "Unable to locate a creek.";
+        }
+
+        return creekID;
+    }
+
+    private String emergencySiteID() {
+        String emergencySiteID;
+
+        try {
+            emergencySiteID = map.getEmergencySiteID();
+        } catch (NoSuchElementException e) {
+            emergencySiteID = "Unable to locate emergency site.";
+        }
+
+        return emergencySiteID;
+    }
+}
