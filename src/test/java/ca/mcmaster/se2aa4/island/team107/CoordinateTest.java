0f8cec48ce3a59eac6f768d2df7477f3ce4ba3ca
bilalm04 <bilal71612@gmail.com>
2024-03-19 17:59:43 -0400

    added new tests for coordinate class

--- src/test/java/ca/mcmaster/se2aa4/island/team107/CoordinateTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team107/CoordinateTest.java
@@ -1,25 +1,53 @@
 package ca.mcmaster.se2aa4.island.team107;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ca.mcmaster.se2aa4.island.team107.position.Coordinate;
 
-import org.junit.jupiter.api.BeforeEach;
-
 public class CoordinateTest {
     private Coordinate coordOne;
     private Coordinate coordTwo;
+    private Coordinate coordThree;
 
     @BeforeEach
     public void setUp() {
         coordOne = new Coordinate(0, 0);
         coordTwo = new Coordinate(1, 49);
+        coordThree = new Coordinate(1, 0);
     }
 
     @Test
-    public void testDistanceTo() {
+    public void testDistanceToTrue() {
         assertEquals(coordOne.distanceTo(coordTwo), Math.sqrt(2402));
+    }
+
+    @Test
+    public void testDistanceToFalse() {
+        assertNotEquals(coordOne.distanceTo(coordThree), 0);
+    }
+
+    @Test
+    public void testGetX() {
+        assertEquals(1, coordThree.getX());
+    }
+
+    @Test
+    public void testGetY() {
+        assertEquals(49, coordTwo.getY());
+    }
+
+    @Test
+    public void testSetX() {
+        coordOne.setX(9);
+        assertEquals(coordOne.getX(), 9);
+    }
+
+    @Test
+    public void testSetY() {
+        coordOne.setY(197);
+        assertEquals(coordOne.getY(), 197);
     }
 }
